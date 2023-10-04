cea14b22d62428b5bcc21d5209fadfef3d2d403f
Matt Brooks <62960175+MattBrooks7@users.noreply.github.com>
2023-10-04 09:11:09 -0500

    second class commit

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -6,15 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./question.component.css']
 })
 export class QuestionComponent {
-  questions: any[] = []
+  questions: any[] = JSON.parse(localStorage.getItem('qStorage') || '[]');
+  // limit to 10 (9)
   questionCreated = false;
-  newQuestion = {question: '', answer1: '', answer2: '', answer3: '', answer4: ''}
+  newQuestion = {question: '', answer1: '', answer2: '', answer3: '', answer4: '', correctAnswer: ''}
+
+
 
 
   submitQuestion() {
-    this.questions.push(this.newQuestion)
-    console.log(this.newQuestion)
-    // return this.newQuestion
+    this.questions.push({...this.newQuestion})
+    localStorage.setItem('qStorage', JSON.stringify(this.questions))
+
+    this.newQuestion = {question: '', answer1: '', answer2: '', answer3: '', answer4: '', correctAnswer: ''}
+  }
+
+  submitQuiz() {
 
   }
+
+  clear() {
+    this.questions = [];
+    localStorage.setItem('qStorage', JSON.stringify(this.questions))
+  }
 }
+
